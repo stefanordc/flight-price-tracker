5b75b0bcbf64a3d244d042b65f6b517c60a214b1
stefanordc <stefanobrunofaria@gmail.com>
2026-03-12 11:50:58 -0300

    setup inicial do projeto playwright

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,20 @@
-print('Hello world')
+from playwright.sync_api import sync_playwright
+
+
+def test_browser():
+
+    with sync_playwright() as p:
+
+        browser = p.chromium.launch(headless=False)
+
+        page = browser.new_page()
+
+        page.goto("https://www.google.com")
+
+        print(page.title())
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    test_browser()
